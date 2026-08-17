facfc82b66f668659430ec8bfcff402be93d2c94
karthik <karthikgudisetti@gmail.com>
2026-08-18 00:44:54 +0530

    Time: 0 ms (100%), Space: 52.5 MB (95.77%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,27 +1,56 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
 
-        int left = 0;
-        int right = nums.length - 1;
+        int left = 1;
+        int right = nums.length - 2;
 
-        while (left < right) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        if (nums[0] != nums[1]) {
+            return nums[0];
+        }
+        if (nums[nums.length - 1] != nums[nums.length - 2]) {
+            return nums[nums.length - 1];
+        }
+        while (left <= right) {
 
-            int mid = left + (right - left) / 2;
+            int mid = (right+left) / 2;
 
-            if (mid % 2 == 1) {
-                mid--;
+            if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
+                return nums[mid];
             }
 
-           
-            if (nums[mid] == nums[mid + 1]) {
-                left = mid + 2;
-            } 
-           
+            if ((mid % 2 == 1 && nums[mid] == nums[mid - 1]) || (mid % 2 == 0 && nums[mid] == nums[mid + 1])) {
+                left = mid + 1;
+            }
+
             else {
-                right = mid;
+                right = mid-1;
             }
         }
 
-        return nums[left];
+        return -1;
+        // int n=arr.length-1;
+        // for(int i=0;i<=n;i++){
+        //     if(i==0){
+        //         if(arr[i]!=arr[i+1]){
+        //             return arr[i];
+        //         }
+        //     }
+        //     else if(i==n){
+        //         if(arr[i]!=arr[i-1]){
+        //             return arr[n];
+        //         }
+        //     }
+        //     else{
+        //         if(arr[i] != arr[i-1] && arr[i]!=arr[i+1]){
+        //             return arr[i];
+        //         }
+        //     }
+
+        // }
+        // return -1;
+
     }
 }
