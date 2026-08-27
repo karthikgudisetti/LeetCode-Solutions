2063be2726c042a4739ad1d30e813a425ea536c3
karthik <karthikgudisetti@gmail.com>
2026-08-27 10:26:09 +0530

    Time: 3 ms (93.12%), Space: 48.5 MB (89.31%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
@@ -1,19 +1,18 @@
 class Solution {
     public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
-      int usum=0;
-      for(int i=0;i<minutes;i++){
-        usum+=customers[i]*grumpy[i];
-      }
-      int msum=usum;
-      for(int i=minutes;i<customers.length;i++){
-        usum+=(customers[i]*grumpy[i])-(customers[i-minutes]*grumpy[i-minutes]);
-        msum=Math.max(msum,usum);
-        
-      }
-      int total=msum;
-      for(int i=0;i<customers.length;i++){
-        total+=customers[i]*(1-grumpy[i]);
-      }
+     int unsatisfied=0;
+     for(int i=0;i<minutes;i++){
+        unsatisfied+=customers[i]*grumpy[i];
+     }
+     int mun=unsatisfied;
+     for(int i=minutes;i<customers.length;i++){
+        unsatisfied+=(customers[i]*grumpy[i])-(customers[i-minutes]*grumpy[i-minutes]);
+        mun=Math.max(mun,unsatisfied);
+     }
+     int total=mun;
+     for(int i=0;i<customers.length;i++){
+        total+=(customers[i]*(1-grumpy[i]));
+     }
       return total;
        
 
